4aaeba63daf6117c098322998a6bda4c1a853162
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2026-01-16 21:32:30 +0100

    Add files via upload

--- main.pl
+++ main.pl
@@ -1,6 +1,21 @@
 napisz  "Witaj|Świecie";
-zmienna musztarda = 2,5 + 3;
+zmienna musztarda = 2,5 + 3+;
 zmienna Mango = 100010"";
+
+zmienna fajna = 1+1+3;
+
+jezeli fajna > 2 ;
+
+:meow;
+
+komentarz jefsfsdfbaf;
+
 napisz Mango;
-zmienna musztard2 = musztarda*2;
+
+
+do ;
+
+zmienna musztard2 = musztarda*2+;
 napisz musztard2;
+
+//idzDo meow;
